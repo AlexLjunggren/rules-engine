c4fffb31111fc61608bee3c6415c25640011a893
aljunggren <aljunggren@allegient.com>
2022-01-22 10:12:07 -0500

    Package name update

--- src/main/java/io/ljunggren/rulesEngine/ConflictResolution.java
+++ src/main/java/io/ljunggren/rulesEngine/ConflictResolution.java
@@ -1,4 +1,4 @@
-package com.ljunggren.rulesEngine;
+package io.ljunggren.rulesEngine;
 
 import java.util.List;
 
--- src/main/java/io/ljunggren/rulesEngine/Rule.java
+++ src/main/java/io/ljunggren/rulesEngine/Rule.java
@@ -1,4 +1,4 @@
-package com.ljunggren.rulesEngine;
+package io.ljunggren.rulesEngine;
 
 public interface Rule<I, O> {
 
